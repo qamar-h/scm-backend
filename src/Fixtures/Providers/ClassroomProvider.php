6daaffa120b3434aaf3d469ba793a3eb0b8b4895
toutvukantabu <gwendal.bescont@gmail.com>
2022-01-11 10:34:22 +0100

    update classroom provider

--- src/Fixtures/Providers/ClassroomProvider.php
+++ src/Fixtures/Providers/ClassroomProvider.php
@@ -2,43 +2,39 @@
 
 namespace App\Fixtures\Providers;
 
-
 use Faker\Provider\Base as BaseProvider;
 
 final class ClassroomProvider extends BaseProvider
 {
-   /**
-    * @var array List of job titles.
-    */
-   const NAME_PROVIDER = [
-       'name' => [
-           'Mathématiques',
-           'Français',
-           'Arabe',
-           'Breton',
-           'Anglais',
-           'SVT',
-           'Physique',
-           'Histoire',
-           'Dessin',
-           'Musique'
-       ]
-   ];
+    /**
+     * @var array List of job titles.
+     */
+    private const NAME_PROVIDER = [
+        'name' => [
+            'Mathématiques',
+            'Français',
+            'Arabe',
+            'Breton',
+            'Anglais',
+            'SVT',
+            'Physique',
+            'Histoire',
+            'Dessin',
+            'Musique'
+        ]
+    ];
 
-   /**
-    * @return string Random job title.
-    */
-   public function classroomName():string
-   {
-       $names = [
-           sprintf(
-               '%s',
-               self::randomElement(self::NAME_PROVIDER['name']),
-
-           ),
-        
-       ];
-
-       return self::randomElement($names);
-   }
+    /**
+     * @return string Random job title.
+     */
+    public function classroomName(): string
+    {
+        $names = [
+            sprintf(
+                '%s',
+                self::randomElement(self::NAME_PROVIDER['name']),
+            ),
+        ];
+        return self::randomElement($names);
+    }
 }
